f11713c09faf737552426e5ca521f2b1f1d5bbcb
rchalla67 <rishichalla67@gmail.com>
2024-01-23 22:35:19 -0500

    refinining analytics page

--- src/components/analyticsDashboard.js
+++ src/components/analyticsDashboard.js
@@ -30,49 +30,44 @@ const AnalyticsDashboard = () => {
             <div className="text-3xl font-bold text-white mb-4">Ophir Statistics</div>
                 <div className="
                 grid 
-                grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 
-                gap-4
+                grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6
                 ">
                     {/* Ophir Price */}
-                    <div className="bg-yellow-400 text-black rounded-lg p-2 md:p-4 shadow-md w-full max-w-[100px] md:max-w-[200px] m-2 flex flex-col items-center justify-center">
-                        <img src="https://raw.githubusercontent.com/cosmos/chain-registry/master/migaloo/images/ophir.png" alt="Icon" className="h-6 w-6 mb-1 md:h-8 md:w-8 md:mb-2" />
-                        <div className="text-lg md:text-2xl font-bold mb-1">Price</div>
-                        <div className="text-md md:text-xl">${0}</div>
+                    <div className="bg-yellow-400 text-black rounded-lg p-4 shadow-md min-w-[100px] m-2 flex flex-col items-center justify-center">
+                        <img src="https://raw.githubusercontent.com/cosmos/chain-registry/master/migaloo/images/ophir.png" alt="Icon" className="h-8 w-8 mb-2" />
+                        <div className="sm:text-2xl text-lg font-bold mb-1">Price</div>
+                        <div className="text-xl sm:text-lg">${0}</div>
                     </div>
                     {/* Market Cap */}
-                    <div className="bg-yellow-400 text-black rounded-lg p-2 md:p-4 shadow-md w-full max-w-[100px] md:max-w-[200px] m-2 flex flex-col items-center justify-center">
-                        <img src="https://raw.githubusercontent.com/cosmos/chain-registry/master/migaloo/images/ophir.png" alt="Icon" className="h-6 w-6 mb-1 md:h-8 md:w-8 md:mb-2" />
-                        <div className="text-lg md:text-2xl font-bold mb-1">Market Cap</div>
-                        <div className="text-md md:text-xl">${0}</div>
+                    <div className="bg-yellow-400 text-black rounded-lg p-4 shadow-md min-w-[100px] m-2 flex flex-col items-center justify-center">
+                        <img src="https://static.thenounproject.com/png/3313489-200.png" alt="Icon" className="h-8 w-8 mb-2" />
+                        <div className="sm:text-2xl text-lg font-bold mb-1">Market Cap</div>
+                        <div className="text-xl">${0}</div>
                     </div>
                     {/* FDV */}
-                    <div className="bg-yellow-400 text-black rounded-lg p-2 md:p-4 shadow-md w-full max-w-[100px] md:max-w-[200px] m-2 flex flex-col items-center justify-center">
-                        <img src="https://raw.githubusercontent.com/cosmos/chain-registry/master/migaloo/images/ophir.png" alt="Icon" className="h-6 w-6 mb-1 md:h-8 md:w-8 md:mb-2" />
-                        <div className="text-lg md:text-2xl font-bold mb-1">FDV</div>
-                        <div className="text-md md:text-xl">${0}</div>
+                    <div className="bg-yellow-400 text-black rounded-lg p-4 shadow-md min-w-[100px] m-2 flex flex-col items-center justify-center">
+                        <img src="https://static.thenounproject.com/png/70884-200.png" alt="Icon" className="h-8 w-8 mb-2" />
+                        <div className="sm:text-2xl text-lg font-bold mb-1">FDV</div>
+                        <div className="text-xl">${0}</div>
                     </div>
                     {/* Circulating Supply */}
-                    <div className="bg-yellow-400 text-black rounded-lg p-2 md:p-4 shadow-md w-full max-w-[100px] md:max-w-[200px] m-2 flex flex-col items-center justify-center">
-                        <img src="https://raw.githubusercontent.com/cosmos/chain-registry/master/migaloo/images/ophir.png" alt="Icon" className="h-6 w-6 mb-1 md:h-8 md:w-8 md:mb-2" />
-                        <div className="text-lg md:text-2xl font-bold mb-1">Circulating Supply</div>
-                        <div className="text-md md:text-xl">0</div>
+                    <div className="bg-yellow-400 text-black rounded-lg p-4 shadow-md min-w-[100px] m-2 flex flex-col items-center justify-center">
+                        <img src="https://static.thenounproject.com/png/3313489-200.png" alt="Icon" className="h-8 w-8 mb-2" />
+                        <div className="sm:text-2xl text-lg font-bold mb-1">Circulating Supply</div>
+                        <div className="text-xl">${0}</div>
                     </div>
                     {/* Staked Supply */}
-                    <div className="bg-yellow-400 text-black rounded-lg p-2 md:p-4 shadow-md w-full max-w-[100px] md:max-w-[200px] m-2 flex flex-col items-center justify-center">
-                        <img src="https://raw.githubusercontent.com/cosmos/chain-registry/master/migaloo/images/ophir.png" alt="Icon" className="h-6 w-6 mb-1 md:h-8 md:w-8 md:mb-2" />
-                        <div className="text-lg md:text-2xl font-bold mb-1">Staked Supply</div>
-                        <div className="text-md md:text-xl">0</div>
+                    <div className="bg-yellow-400 text-black rounded-lg p-4 shadow-md min-w-[100px] m-2 flex flex-col items-center justify-center">
+                        <img src="https://static.thenounproject.com/png/3313489-200.png" alt="Icon" className="h-8 w-8 mb-2" />
+                        <div className="sm:text-2xl text-lg font-bold mb-1">Staked Supply</div>
+                        <div className="text-xl">${0}</div>
                     </div>
                     {/* Total Supply */}
-                    <div className="bg-yellow-400 text-black rounded-lg p-2 md:p-4 shadow-md w-full max-w-[100px] md:max-w-[200px] m-2 flex flex-col items-center justify-center">
-                        <img src="https://raw.githubusercontent.com/cosmos/chain-registry/master/migaloo/images/ophir.png" alt="Icon" className="h-6 w-6 mb-1 md:h-8 md:w-8 md:mb-2" />
-                        <div className="text-lg md:text-2xl font-bold mb-1">Price</div>
-                        <div className="text-md md:text-xl">1,000,000,000</div>
+                    <div className="bg-yellow-400 text-black rounded-lg p-4 shadow-md min-w-[100px] m-2 flex flex-col items-center justify-center">
+                        <img src="https://static.thenounproject.com/png/3313489-200.png" alt="Icon" className="h-8 w-8 mb-2" />
+                        <div className="sm:text-2xl text-lg font-bold mb-1">Total Supply</div>
+                        <div className="sm:text-xl text-lg ">1,000,000,000</div>
                     </div>
-                <Card title="Circ. Supply" value="$xxxxxx" additionalInfo="xx% of Total Supply" />
-                <Card title="Whale price" value="$xxxxxx" />
-                <Card title="Total Treasury Assets Value (USD)" value="$xxxxxxxxxxxxxx" />
-                <Card title="Staked Supply" value="xxxxx" additionalInfo="xx% of Circ. Supply" />
                 </div>
             </div>
         </div>
