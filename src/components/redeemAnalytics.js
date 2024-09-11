5fa9b5cf0093ac9d45d09ca586354be40bb6c99f
rchalla67 <rishichalla67@gmail.com>
2024-09-11 19:59:02 -0400

    added redeem-analytics page

--- src/components/redeemAnalytics.js
+++ src/components/redeemAnalytics.js
@@ -64,12 +64,9 @@ const ToggleableAmount = ({ amount, value }) => {
 };
 
 const LoadingSpinner = () => (
-  <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
-    <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
-    <h2 className="text-center text-white text-xl font-semibold">Loading...</h2>
-    <p className="w-1/3 text-center text-white">
-      This may take a few seconds, please don't close this page.
-    </p>
+  <div className="flex flex-col justify-center items-center h-screen">
+    <div className="text-white mb-4">Fetching On-Chain Data...</div>
+    <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-yellow-400"></div>
   </div>
 );
 
